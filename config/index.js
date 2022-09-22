7bdb429cb9271e18b2901c00eb91f40bfcb5c050
zaoanxiaoh <114168249+zaoanxiaoh@users.noreply.github.com>
2022-09-23 07:35:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -57,12 +57,12 @@ export const USER_CONFIG = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [f4be24ea0177ac0abb26de38c152541d
+  USERS: 
     {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oU7lv54pvvETUcn4To8grrxcWrPs",
+      id: "oU7lv54gN5oHLSZYbL_tyvupG8so",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "GAhuV-VmVACoZc8TZN1SclXXIahm51nVTK1aVFCaGuE	",
       // 所在省份
